bc939e432173144030c94cfd5b967d9887f659d3
Boin Kim <charles1236@kau.kr>
2022-02-06 17:13:19 +0900

    220206 HTML 및 프론트단 JS 작업

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('hello, i am login');
+const id = document.querySelector('#input-id');
+const pw = document.querySelector('#input-pw');
+const loginBtn = document.querySelector('#btn-login');
+
+const handleLoginClick = () => {
+  const req = {
+    id: id.value,
+    pw: pw.value,
+  };
+
+  
+}
+
+loginBtn.addEventListener('click', handleLoginClick);
